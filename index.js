1f76c091c1bf592e1df57b21b9cba18af654c494
siraj9949 <shaiksirajuddin9949@gmail.com>
2023-12-25 17:26:00 +0530

    post

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const app = express();
 const port = 3000;
 
 // Endpoint that sends a JSON response
-app.get("/", (req, res) => {
-  const jsonData = {
-    message: "Hello, this is a JSON response!",
-    timestamp: new Date().toISOString(),
-  };
-  res.json(jsonData);
-});
+app
+  .get("/", (req, res) => {
+    const jsonData = {
+      message: "Hello, this is a JSON response!",
+      timestamp: new Date().toISOString(),
+    };
+    res.json(jsonData);
+  })
+  .post((req, res) => {
+    const details = req.body;
+    console.log("Received details:", details);
+    res.json({ message: "Details logged successfully" });
+  });
 
 // Start the server
 app.listen(port, () => {
